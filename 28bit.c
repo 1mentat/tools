e51ff08113975dc288bfb49e7a70c72cd8ff4b22
James Burns <jfburns@gmail.com>
2012-11-02 12:17:33 -0700

    dual rotation

--- 28bit.c
+++ 28bit.c
@@ -4,21 +4,24 @@
 int
 main()
 {
-    uint64_t item = 0x0000000000000001;
-    uint64_t mask = 0xfffffffff0000000;
-    uint64_t clear = 0x000000000fffffff;
+    uint64_t item =             0x00a5a5a5a5a5a5a5;
+    uint64_t mask_upper =       0x00fffffff0000000;
+    uint64_t mask_lower =       0x000000000fffffff;
+    uint64_t mask_bits =        0x0100000010000000;
+    uint64_t bits =             0x0000000000000000;
+
     int i;
 
-    printf("0x%llx\n", item);
+    printf("0x%llx 0x%llx\n", ((item & mask_upper) >> 28), item & mask_lower);
 
     for (i = 0; i < 56; i++)
     {
         item <<= 1;
-        printf("0x%llx\n", item);
-        printf("shifted off 0x%llx\n", item & mask);
-        printf("wrapped 0x%llx\n", item ^ ((item & mask) >> 28));
-        item = ((item ^ ((item & mask) >> 28)) & clear);
-        printf("cleared 0x%llx\n", item);
+        bits = item & mask_bits;
+        bits >>= 28;
+        item &= ~(mask_bits >> 28);
+        item |= bits;
+        printf("rotate: %d 0x%llx 0x%llx\n", i, ((item & mask_upper) >> 28), item & mask_lower);
     }
 
     return 0;
