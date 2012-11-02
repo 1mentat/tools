60aacd0ca3234e2065a7d9a7a06b77b21c4b3737
James Burns <jfburns@gmail.com>
2012-11-02 12:24:10 -0700

    adding right rotation

---
+++ 28bitr.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int
+main()
+{
+    uint64_t item =             0x00a5a5a5a5a5a5a5;
+    uint64_t mask_upper =       0x00fffffff0000000;
+    uint64_t mask_lower =       0x000000000fffffff;
+    uint64_t mask_bits =        0x0000000010000001;
+    uint64_t bits =             0x0000000000000000;
+
+    int i;
+
+    printf("0x%llx 0x%llx\n", ((item & mask_upper) >> 28), item & mask_lower);
+
+    for (i = 0; i < 56; i++)
+    {
+        bits = item & mask_bits;
+        item >>= 1;
+        bits <<= 27;
+        item &= ~(mask_bits << 27);
+        item |= bits;
+        printf("rotate: %d 0x%llx 0x%llx\n", i, ((item & mask_upper) >> 28), item & mask_lower);
+    }
+
+    return 0;
+}
